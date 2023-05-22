6be62dc82e00cf51261c68cd0f535be87e7e213d
Pranjal Singh <65503194+pranjiz@users.noreply.github.com>
2023-05-22 20:59:47 +0530

    Add files via upload

---
+++ JS6_Code_Challenge/js/main.js
@@ -0,0 +1,16 @@
+// Your First Code Challenge
+
+// Write code that will return a random letter from your name
+console.log(typeof "Pranjal");
+console.log("Pranjal".charAt());
+console.log(Math.floor(Math.random() * 4 + 1));
+console.log("Dave".charAt(Math.floor(Math.random() * 7)));
+// Let's make this work for ALL names
+const anyName = "Jonathan";
+// What about the number
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+console.log(anyName.charAt(Math.floor(Math.random() * anyName.length)));
+
